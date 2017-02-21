703020846adf10b7cbcea6b079c5b5d100fc9e53
LoeiFy <LoeiFy@gmail.com>
2017-02-21 15:52:43 +0800

    indent 2 and remove test

--- test/test.js
+++
@@ -1,9 +0,0 @@
-require('should')
-
-const { convert } = require('../lib/util')
-
-describe('transform text', () => {
-    it('should get "zhong_wen"', () => {
-        convert('中文').should.be.eql('zhong_wen')
-    })
-})
