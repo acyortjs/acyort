0d89f8e92d09b9036902d0451a3a122a0d1f782b
LoeiFy <LoeiFy@gmail.com>
2017-06-13 00:23:37 +0800

    helper

--- test.js
+++ test.js
@@ -14,3 +14,29 @@
 //     console.log(this.config)
 //   }
 // }
+
+const I18n = require('acyort-i18n')
+const pathFn = require('path')
+const yaml = require('yamljs')
+
+class Test extends I18n {
+  constructor() {
+    const i18n = {}
+    super({
+      locales: ['en'],
+      register: i18n,
+      directory: pathFn.join(process.cwd(), 'assets/themes/ccc45', 'i18n'),
+      extension: '.yml',
+      parse: data => yaml.parse(data.toString('utf-8')),
+    })
+    this.i18n = i18n
+  }
+
+  log() {
+    console.log(this.i18n)
+  }
+}
+
+var s = new Test()
+
+s.log()
