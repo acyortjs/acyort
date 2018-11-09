8dcb783ccd454ba174e5576c5f306e670dda3748
LoeiFy <LoeiFy@gmail.com>
2018-11-09 21:11:57 +0800

    init func

--- bin/acyort.js
+++ bin/acyort.js
@@ -4,8 +4,9 @@ const extend = require('@acyort/extender')
 const logger = require('@acyort/logger')()
 const { join } = require('path')
 const { readdirSync, existsSync } = require('fs')
-const cli = require('../lib/cli')
 const parser = require('../lib/cli/parser')
+const Acyort = require('../lib')
+const getConfig = require('../lib/config')
 
 const argv = process.argv.slice(2)
 
@@ -13,13 +14,15 @@ if (argv[0] !== 'init') {
   try {
     const cwd = process.cwd()
     const scriptsDir = join(cwd, 'scripts')
+    const config = getConfig(cwd)
+    const acyort = new Acyort(config)
 
     if (existsSync(scriptsDir)) {
       readdirSync(join(cwd, 'scripts'))
         .filter(name => name.indexOf('.js') > -1)
         .forEach((name) => {
           const path = join(cwd, 'scripts', name)
-          extend(path, { cli }, 'acyort')
+          extend(path, acyort, 'acyort')
         })
     }
   } catch (e) {
