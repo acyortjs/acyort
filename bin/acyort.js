cff19461dcdb1378887a3d6e94babfef02cdf642
LoeiFy <LoeiFy@gmail.com>
2018-11-10 15:48:30 +0800

    cli

--- bin/acyort.js
+++ bin/acyort.js
@@ -10,24 +10,30 @@ const getConfig = require('../lib/config')
 
 const argv = process.argv.slice(2)
 const base = process.cwd()
+const ignores = ['init', '-h', '--help', '-v', '--version']
 
-if (argv[0] !== 'init') {
+if (argv[0] && !ignores.includes(argv[0])) {
   try {
     const config = getConfig(base)
     const scriptsDir = join(base, 'scripts')
-    const acyort = new Acyort(config)
 
-    if (existsSync(scriptsDir)) {
-      readdirSync(scriptsDir)
-        .filter(name => name.indexOf('.js') > -1)
-        .forEach((name) => {
-          const path = join(base, 'scripts', name)
-          extend(path, acyort, 'acyort')
-        })
+    if (config) {
+      const acyort = new Acyort(config)
+
+      if (existsSync(scriptsDir)) {
+        readdirSync(scriptsDir)
+          .filter(name => name.indexOf('.js') > -1)
+          .forEach((name) => {
+            const path = join(base, 'scripts', name)
+            extend(path, acyort, 'acyort')
+          })
+      }
+
+      parser(argv)
     }
   } catch (e) {
     logger.error(e)
   }
+} else {
+  parser(argv)
 }
-
-parser(argv)
