de2a869f2aeb563b58577146b30e340773c317cb
zhengzhirong <zhengzwing@gmail.com>
2019-01-07 19:11:01 +0800

    feat(Store): support namespace

--- test/store.js
+++ test/store.js
@@ -2,18 +2,46 @@ const assert = require('power-assert')
 const Store = require('../lib/store')
 
 describe('store', () => {
-  it('test', () => {
-    const store = new Store()
+  const stores = new Store()
+  it('test global store', () => {
+    stores.set('key', 'key')
+    assert(stores.global.store.key === 'key')
 
+    stores.set('key', 'other')
+    assert(stores.global.store.key === 'other')
+
+    assert(stores.get('key') === 'other')
+
+    stores.reset()
+    assert(stores.get('key') === undefined)
+  })
+
+  it('test register store', () => {
+    const namespace = 'kvjzlkjv'
+    stores.register(namespace)
+    const store = stores.bind(namespace)
+    assert(store === stores.modules[namespace])
+  })
+  it('test namespace store', () => {
+    const namespace = 'fdsafdaf'
+    const store = stores.bind(namespace)
     store.set('key', 'key')
-    assert(store.store.key === 'key')
+    assert(store.get('key') === 'key')
+    assert(stores.get(`${namespace}:key`) === 'key')
+    const other = 'ooooooo'
+    stores.set(`${namespace}:other`, other)
+    assert(store.get('other') === other)
+    assert(stores.get(`${namespace}:other`) === other)
 
-    store.set('key', 'other')
-    assert(store.store.key === 'other')
+    stores.reset(namespace)
+    assert(store.get('other') === undefined)
+    assert(stores.get(`${namespace}:other`) === undefined)
+  })
 
-    assert(store.get('key') === 'other')
-
-    store.reset()
-    assert(store.get('key') === undefined)
+  it('test get store without register', () => {
+    const namespace = '1231651'
+    const valu = 'valu'
+    stores.set(`${namespace}:key`, valu)
+    assert(stores.get(`${namespace}:key`) === valu)
   })
 })
