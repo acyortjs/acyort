e44b9975869b49c47f22c9762eafe61e10feba6a
LoeiFy <LoeiFy@gmail.com>
2019-01-12 11:57:36 +0800

    test: template from npm

--- test/config.js
+++ test/config.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require('fs-extra')
+const { writeFileSync, removeSync, ensureDirSync } = require('fs-extra')
 const { join } = require('path')
 const assert = require('power-assert')
 const getConfig = require('../lib/config')
@@ -6,10 +6,17 @@ const getConfig = require('../lib/config')
 const config0 = 'url:'
 const config1 = 'url: https://test.com/public'
 const config2 = 'url: test.com'
+const config3 = 'template: npm'
 
 describe('config', () => {
+  const base = join(__dirname, 'fixtures')
+
+  afterEach(() => {
+    removeSync(join(base, 'config.yml'))
+    removeSync(join(base, 'node_modules'))
+  })
+
   it('test', () => {
-    const base = join(__dirname, 'fixtures')
     let config
 
     writeFileSync(join(base, 'config.yml'), config0)
@@ -30,8 +37,16 @@ describe('config', () => {
     assert(config.url === 'https://acyort.com')
     assert(config.root === '/')
 
-    config = getConfig()
+    writeFileSync(join(base, 'config.yml'), config3)
+    config = getConfig(base)
 
+    assert(config.templatePath === undefined)
+
+    ensureDirSync(join(base, 'node_modules', 'npm', 'templates', 'npm'))
+    config = getConfig(base)
+    assert(config.templatePath === join(base, 'node_modules', 'npm', 'templates', 'npm'))
+
+    config = getConfig()
     assert(config === null)
   })
 })
