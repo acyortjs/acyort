9045d17fcffefcbdec4445e0b64df1fc4a0d2aee
loeify <loeify@gmail.com>
2023-07-05 22:46:00 +0800

    fix: jest

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,19 @@
+const transform = {
+  '^.+\\.ts?$': 'ts-jest',
+}
+
 module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: ['/node_modules/'],
-  moduleFileExtensions: ['ts', 'js', 'json'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.json',
-      diagnostics: false,
-      useESM: true,
-    },
-  },
   collectCoverage: true,
   projects: [
     {
       displayName: 'i18n',
       roots: ['<rootDir>/packages/i18n'],
+      // https://github.com/jestjs/jest/issues/11411
+      transform,
     },
   ],
 }
--- packages/i18n/test/index.test.ts
+++ packages/i18n/test/index.test.ts
@@ -1,5 +1,9 @@
-describe('test', () => {
-  it('hook', () => {
-    expect('a').toHaveLength(1)
+import { join } from 'path'
+import I18n from '../src'
+
+describe('i18n', () => {
+  it('cases', () => {
+    const i18n = new I18n(join(__dirname, 'locales'), 'default')
+    expect(i18n.locale).toBe('default')
   })
 })
